8468d33fb78691a6672362447b049003cba01397
VG_VITIN <kaiomendess1111@gmail.com>
2025-02-10 20:26:28 -0300

    aula 10/02

--- index.php
+++ index.php
@@ -6,8 +6,51 @@
     <title>Portifolio</title>
 
         <link rel="stylesheet" href="css/style.css" />
+        
 </head>
 <body>
     
+<div class="interface">
+
+    <header class="cabecalho">
+
+    <nav class="menu">
+
+        <div class="logo">
+            <img src="imagens/logo.png" alt="LOGO" width= "150" />
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>Serviços</li>
+            <li>Produtos</li>
+            <li>Portifolio</li>
+            <li>Sobre</li>
+            <li>Contato</li>
+            <li></li>
+        </ul>
+
+
+    </nav>
+    <div class="titles">
+
+        <h1 class="titulo">Entregando serviços de qualidade</h1>
+        <h1 class="subtitulo">Soluções em TI</h1>
+
+        <p class="paragrafo">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, vero vitae provident officiis harum recusandae!</p>
+    </div>
+
+    <div class="socialIcons">
+
+        <a href="#">Twitter</a>
+        <a href="#">Facebook</a>
+        <a href="#">LinkeDin</a>
+        <a href="#">Instagram</a>
+
+    </div>
+    </header>
+
+</div>
+
 </body>
 </html>
