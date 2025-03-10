930100b24325305b2070a2395b94ac2c4e7012d2
VG_VITIN <kaiomendess1111@gmail.com>
2025-03-10 20:43:43 -0300

    aula 10/03

--- index.php
+++ index.php
@@ -1,60 +1,57 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8 /">
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
-    <title>Portifolio</title>
+    <title>Portifólio</title>
 
-       <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="tools/fontawesome/css/all.css" />
+    <link rel="stylesheet" href="css/style.css" />
+
 </head>
 <body>
-    
+
 <div id="interface">
 
     <header class="cabecalho">
 
+        <nav class="menu">
+
+            <div class="logo">
+                <img src="imagens/logo.png" alt="LOGO" />
+            </div>
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Portifólio</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+
+        </nav>
         
-
-    <nav class="menu">
-
-        <div class="logo">
-            <img src="imagens/logo.png" alt="LOGO" width= "150" />
-        </div>
-
-        <ul>
-            <li><a href= "#">Home</a></li>
-            <li><a href= "#">Serviços</a></li>
-            <li><a href= "#">Produtos</a></li>
-            <li><a href= "#">Portifolio</a></li>
-            <li><a href= "#">Sobre</a></li>
-            <li><a href= "#">Contato</a></li>
-        </ul>
-
-
-    </nav>
-        <div class="titles">
-
+        <div class="titles"> <!-- ========================= -->
             <div class="container-titles">
-
-                <h1 class="titulo">Entregando serviços de qualidade</h1>
+                <h1 class="titulo">Entregando Serviços de Qualidade</h1>
                 <h1 class="subtitulo">Soluções em TI</h1>
 
-                <p class="paragrafo">O amor é um sentimento que ninguem jamais irá conseguir explicar e entender-la por completa!</p>
-            
+                <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vitae rerum, necessitatibus quod quaerat mollitia.</p>
+                
             </div>
 
             <div class="socialIcons">
-
-                <a href="#">Twitter</a>
-                <a href="#">Facebook</a>
-                <a href="#">LinkeDin</a>
-                <a href="#">Instagram</a>
+                <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
+                <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
+                <a href="#" title="Visite nosso LinkedIn"><i class="fab fa-linkedin"></i></a>
+                <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
             </div>
-        </div>
+        </div> <!-- ========================== -->
+        
     </header>
 
 </div>
-
+    
 </body>
 </html>
